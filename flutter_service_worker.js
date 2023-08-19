b7e7dc86ce29edfd4156ca952c6376c28ce2d3f3
tlotterWSA <139960051+tlotterWSA@users.noreply.github.com>
2023-08-19 11:29:13 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,8 +34,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "5c6560ad1dfab2c93aae36d30879f2b4
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "155fd837c33eea6269d1927d7b12cb1b",
-"/": "155fd837c33eea6269d1927d7b12cb1b",
+"index.html": "1b507e0ed355da1bf245e8b56673b8c9",
+"/": "1b507e0ed355da1bf245e8b56673b8c9",
 "main.dart.js": "70302dbda9012ac2b2607d72d6059b71",
 "manifest.json": "e7480c1a146ba550e64ab8bd75738824",
 "version.json": "ff2b129bc2c61b452b8854392cb858ef"};
